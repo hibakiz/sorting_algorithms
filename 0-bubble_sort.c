dbcde12fe803f8a00d4ddb2f734f867a467fcdb2
hibakiz <hbosalah98sd@gmail.com>
2023-12-14 20:13:24 +0300

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+/**
+* bubble_sort - sort an array with bubble sort algorithm and
+* print the array after each swap
+* @array: integer array to sort
+* @size: the size of the array
+* Return: no return
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	int flag;
+
+	do {
+		flag = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_int(&array[i], &array[i + 1]);
+				flag = 1;
+				print_array(array, size);
+			}
+
+		}
+	} while (flag);
+
+}
+/**
+ * swap_int - Entry point
+ *@a: int
+ *@b: int
+ * Description: 'tsks'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
